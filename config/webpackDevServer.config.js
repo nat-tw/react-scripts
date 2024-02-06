0c1aa85b7a1226861f81a08610c5dca9eb665d83
nat-tw <nat.nganrungrueang@jetstar.com>
2024-02-06 18:50:16 +0700

    add redirectServedPath

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -14,6 +14,7 @@ const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMi
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const paths = require('./paths');
 const fs = require('fs');
+const redirectServedPath = require('react-dev-utils/redirectServedPathMiddleware');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
